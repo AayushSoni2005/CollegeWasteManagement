3cf91764dc721716ffdc2244c499d424095766c1
Aayush Soni <2005.Maheshwari.Aayush.com>
2026-03-27 18:12:49 +0530

    Added new backend1

---
+++ backend1/src/main/java/com/college_clean/CollegeWasteManagement/CollegeWasteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.college_clean.CollegeWasteManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeWasteManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeWasteManagementApplication.class, args);
+	}
+
+}
